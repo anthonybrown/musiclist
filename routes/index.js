e73687ee43164c008fea3ee95b9d12856a7625e1
Tony Brown <tony@tonybrown.me>
2018-05-20 18:29:33 -0400

    setup api

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,9 @@
-const express = require('express');
-const router  = express.Router();
+const express = require('express')
+const router  = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'MusicList' });
-});
-
-const testJSON = [
-  {
-    name: 'Tony Brown',
-    username: 'codebarbarian',
-  },
-
-  {
-    name: 'Ho Cakes',
-    username: 'StormyCakes',
-  }
-]
-
-router.get('/sendjson', (req, res, next) => {
-  res.json(testJSON)
+  res.render('index', { title: 'MusicList' })
 })
 
-module.exports = router;
+module.exports = router
