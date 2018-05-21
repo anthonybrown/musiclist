912bd20f88b6df3cee901c140119b5de1b47fe8f
Tony Brown <tony@tonybrown.me>
2018-05-21 14:06:45 -0400

    dev code

---
+++ callback.js
@@ -0,0 +1,55 @@
+// JS promises
+function addExtra(price) {
+
+  return new Promise(function(resolve, reject) {
+    if (price > 2) reject('Price cannot exceed 3')
+
+    setTimeout(function() {
+      resolve(price + 1)
+    }, 1000)
+  })
+}
+
+addExtra(1)
+  .then(addExtra)
+  .then(addExtra)
+  .then(function(newPrice) {
+  console.log(newPrice);
+}).catch(function(error) {
+  console.log(error);
+})
+
+
+
+// Classic callback 
+// function addExtra(price, cb) {
+//   if (price > 2) {
+//     cb(false, 'Price cannot exceed 3')
+    // return to avoid executing the rest of the function.
+  //   return
+  // }
+
+//   setTimeout(function() {
+//     cb(price + 1)
+//   }, 1000)
+// }
+
+// addExtra(1, function(newPrice, error) {
+//   if (error) {
+//     console.log(error)
+//     return
+//   }
+//   addExtra(newPrice, function(newPrice2, error) {
+//     if (error) {
+//       console.log(error)
+//       return
+//     }
+//     addExtra(newPrice2, function(newPrice3, error) {
+//       if (error) {
+//         console.log(error)
+//         return
+//       }
+//       console.log(newPrice3)
+//     })
+//   })
+// })
