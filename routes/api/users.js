fb7ff0aad5dd61e0ac94ae683a677e7754892caa
Tony Brown <tony@tonybrown.me>
2018-05-24 12:20:14 -0400

    add eslint and a favicon to the repo

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const router  = express.Router()
+const express = require('express');
 
-const User = require('../../models/user')
+const router = express.Router();
+const User   = require('../../models/user');
 
-router.get('/list', (req, res, next) => {
+router.get('/list', (req, res) => {
   User.find((err, users) => {
     if (err) {
-      res.send(err)
+      res.send(err);
     }
-    res.json(users)
-  })
-})
 
-module.exports = router
+    return res.json(users);
+  });
+});
+
+module.exports = router;
+
--- routes/api/users.test.js
+++ routes/api/users.test.js
@@ -1,4 +1,5 @@
-const request = require('supertest')
+/* global describe, it, expect */
+const request = require('supertest');
 
 // top level of this test suite: the entire user API
 describe('The User API', () => {
@@ -9,14 +10,14 @@ describe('The User API', () => {
     const res = await request('http://localhost:3333')
       .get('/api/users/list')
       .expect(200)
-      .expect('Content-Type', /json/)
+      .expect('Content-Type', /json/);
 
     // these expects are jest, not supertest
     // first, expect to get a result that is an array
-    expect(Array.isArray(res.body)).toBe(true)
+    expect(Array.isArray(res.body)).toBe(true);
     // second, expect the array to have something in it.
-    expect(res.body.length).toBeGreaterThan(0)
+    expect(res.body.length).toBeGreaterThan(0);
     // third, expect the username of the first returned user to be `adiminstrator`
-    expect(res.body[0].username).toBe('administrator')
-  })
-})
+    expect(res.body[0].username).toBe('administrator');
+  });
+});
